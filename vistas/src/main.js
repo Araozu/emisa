e5e26df3e45ed949219255ad69227bb594a85d55
Araozu <tbyuyfcjjll@gmail.com>
2020-07-06 21:50:19 -0500

    Agregado Vue3

--- vistas/src/main.js
+++ vistas/src/main.js
@@ -1,12 +1,9 @@
-import Vue from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+const app = createApp(App)
+    .use(router)
+    .use(store)
+    .mount('#app');
