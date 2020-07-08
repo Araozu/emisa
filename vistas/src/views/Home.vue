cbacbce95f803a16b7600875de36c300e18c7637
Araozu <tbyuyfcjjll@gmail.com>
2020-07-08 14:53:28 -0500

    Agregada funcionalidad a boton cerrar de la vista.

--- vistas/src/views/Home.vue
+++ vistas/src/views/Home.vue
@@ -1,66 +1,95 @@
 <template>
     <div>
-        <div class="gzz_astro">
-            <h1>GZZ_ASTRO</h1>
-            <div class="registro">
-                <h3>Registro de Astro</h3>
-                <form @submit.prevent>
-                    <div class="estr_form">
-                        <label for="AstCod">AstCod</label>
-                        <input id="AstCod" type="text" name="AstCod">
-                        <label for="AstNom">AstNom</label>
-                        <input id="AstNom" type="text" name="AstNom">
-                        <label for="AstTip">AstTip</label>
-                        <input id="AstTip" type="text" name="AstTip">
-                    </div>
-                </form>
-            </div>
-            <br>
-            <div class="tabla">
-                <h3>Tabla GZZ_ASTRO</h3>
-                <table class="tabla_datos">
-                    <thead>
-                    <tr>
-                        <td>AstCod</td>
-                        <td>AstNom</td>
-                        <td>AstTip</td>
-                    </tr>
-                    </thead>
-                    <tbody>
+        <template v-if="conexionActiva">
+            <div class="gzz_astro">
+                <h1>GZZ_ASTRO</h1>
+                <div class="registro">
+                    <h3>Registro de Astro</h3>
+                    <form @submit.prevent>
+                        <div class="estr_form">
+                            <label for="AstCod">AstCod</label>
+                            <input id="AstCod" type="number" name="AstCod" v-model="astCod">
+                            <label for="AstNom">AstNom</label>
+                            <input id="AstNom" type="text" name="AstNom" v-model="astNom">
+                            <label for="AstTip">AstTip</label>
+                            <input id="AstTip" type="number" name="AstTip" v-model="astTip">
+                        </div>
+                    </form>
+                </div>
+                <br>
+                <div class="tabla">
+                    <h3>Tabla GZZ_ASTRO</h3>
+                    <table class="tabla_datos">
+                        <thead>
+                        <tr>
+                            <td>AstCod</td>
+                            <td>AstNom</td>
+                            <td>AstTip</td>
+                        </tr>
+                        </thead>
+                        <tbody>
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
+                <br>
+                <div class="botones">
+                    <div>
+                        <button>Adicionar</button>
+                    </div>
+                    <div>
+                        <button>Modificar</button>
+                    </div>
+                    <div>
+                        <button>Elimitar</button>
+                    </div>
+                    <div>
+                        <button>Cancelar</button>
+                    </div>
+                    <div>
+                        <button>Inactivar</button>
+                    </div>
+                    <div>
+                        <button>Reactivar</button>
+                    </div>
+                    <div>
+                        <button>Actualizar</button>
+                    </div>
+                    <div>
+                        <button @click="cerrarVentana">Salir</button>
+                    </div>
+                </div>
             </div>
-            <br>
-            <div class="botones">
-                <div>
-                    <button>Adicionar</button>
-                </div>
-                <div>
-                    <button>Modificar</button>
-                </div>
-                <div>
-                    <button>Elimitar</button>
-                </div>
-                <div>
-                    <button>Cancelar</button>
-                </div>
-                <div>
-                    <button>Inactivar</button>
-                </div>
-                <div>
-                    <button>Reactivar</button>
-                </div>
-                <div>
-                    <button>Actualizar</button>
-                </div>
-                <div>
-                    <button>Salir</button>
-                </div>
-            </div>
-        </div>
+        </template>
+        <template v-else>
+            <p>Conexión terminada. Puedes cerrar esta pestaña.</p>
+        </template>
     </div>
 </template>
+
+<script>
+    import { ref } from "vue";
+
+    export default {
+        setup () {
+            const astCod = ref(-1);
+            const astNom = ref("");
+            const astTip = ref(-1);
+            const conexionActiva = ref(true);
+
+            const cerrarVentana = () => conexionActiva.value = false;
+
+            return {
+                astCod,
+                astNom,
+                astTip,
+                conexionActiva,
+                cerrarVentana
+            }
+        }
+    }
+
+</script>
 
 <style scoped>
 
@@ -89,7 +118,7 @@
         border-radius: var(--tamanio-border-radius);
     }
 
-    .registro h3 {
+    .registro h3, .tabla h3 {
         margin: 0 0 2rem 0;
     }
 
@@ -103,6 +132,10 @@
         text-align: center;
     }
 
+    .botones div button {
+        cursor: pointer;
+    }
+
     .tabla_datos {
         display: table;
         width: 100%;
@@ -110,7 +143,7 @@
     }
 
     .tabla_datos, .tabla_datos td {
-        border: 2px solid #151515;
+        border: 1px solid #151515;
         border-collapse: collapse;
     }
 
