c0082731ac91b61e2ca374a6100d4ca8f7d3372d
Araozu <tbyuyfcjjll@gmail.com>
2020-08-02 13:00:21 -0500

    Arreglado error en el nombre de la tabla rzc_factoria

--- src/api/datos/RZC_Factoria_Empleo_Servlet.java
+++ src/api/datos/RZC_Factoria_Empleo_Servlet.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 
 public class RZC_Factoria_Empleo_Servlet extends EmisaServlet {
 
-    static final String nombreTabla = "r7c_factoria_empleo";
+    static final String nombreTabla = "rzc_factoria_empleo";
     static LinkedList<InfoCampo> campos = new LinkedList<>();
     static final String campoId = "FacEmpCod";
     static final String campoEstReg = "FacEmpEstReg";
